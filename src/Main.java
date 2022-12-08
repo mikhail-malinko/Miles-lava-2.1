e0811e904cb1350707331eb10e62cd16eb5e6750
Mikhail156 <mikhail156@mail.ru>
2022-12-08 20:21:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 35_710;
+        int milePrice = 20;
+        int freeMiles = ticketPrice / milePrice;
+
+        System.out.println("Бонусные мили: " + freeMiles);
+
+    }
+}

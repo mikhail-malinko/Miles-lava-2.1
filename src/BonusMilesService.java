ca9ed091a485c222260ff460d9101ba06c8288a2
Mikhail156 <mikhail156@mail.ru>
2022-12-12 16:02:36 +0300

    mod commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate(int x) {
+
+        int result;
+        {
+            result = x / 20;
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 35_710;
-        int milePrice = 20;
-        int freeMiles = ticketPrice / milePrice;
+//        int ticketPrice = 35_710;
+//        int milePrice = 20;
+//        int freeMiles = ticketPrice / milePrice;
+//
+//        System.out.println("Бонусные мили: " + freeMiles);
 
-        System.out.println("Бонусные мили: " + freeMiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 35_710;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
